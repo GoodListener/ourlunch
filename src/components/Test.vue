765693bac35d41c3d8f698c1623564522a4a1c48
unknown <good_listener@naver.com>
2019-06-03 00:01:58 +0900

    버튼 컴포넌트 등록 slot 사용

---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>test</div>
+    <Button></Button>
+  </div>
+</template>
+
+<script>
+import Button from '@/components/ui/Button.vue'
+
+export default {
+  name: 'Test',
+  components: {
+    Button
+  }
+}
+</script>
