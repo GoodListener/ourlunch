d1f3edfe79cfcaf26179cc7b7e63e0ecab85f7a2
unknown <good_listener@naver.com>
2019-06-09 01:01:21 +0900

    input 태그 디자인 샘플 추가

--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -1,12 +1,22 @@
 <template>
-  <span>
-    <label>{{label}}</label>
-    <input class="base_input" :type=type :value=value />
-  </span>
+  <div :class="type">
+    <label :class="labelText">{{label}}</label>
+    <input
+    class="base_input"
+    :type="type"
+    :value="value"
+    @focus="onfocus()"
+    @blur="onfocusout()" />
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      labelText: 'focusout'
+    }
+  },
   props: {
     type: {
       type: String,
@@ -17,13 +27,41 @@ export default {
       required: true
     },
     label: {
-      type: String
+      type: String,
+      default: 'label'
+    }
+  },
+  methods: {
+    onfocus: function () {
+      this.labelText = 'focusin'
+    },
+    onfocusout: function () {
+      this.labelText = 'focusout'
     }
   }
 }
 </script>
 
 <style scoped>
+div.text {
+  display: inline-flex;
+}
+
+div.text label {
+  position: absolute;
+  display: flex;
+  align-items: flex-start;
+  transition: transform 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms;
+}
+
+.focusout {
+  transform: none;
+}
+
+.focusin {
+  transform: scale(0.75);
+}
+
 input {
   border: 0px;
 }
