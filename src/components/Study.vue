d93d434129ce38a26060d3df375094a294e3d9b2
graceuu <rushel14@hanmail.net>
2019-07-26 18:15:27 +0900

    1. axios 추가 2. 문법 오류 수정

--- src/components/Study.vue
+++ src/components/Study.vue
@@ -1,8 +1,8 @@
 <template>
 <div id="todo-list-example">
   <ul>
-    <li v-for=" famMembers in famMembers">
-    {{ famMembers }}
+    <li v-for="(famMember, index) in famMembers" v-bind:key="index">
+    {{ famMember.name }}
     </li>
   </ul>
 </div>
@@ -16,47 +16,49 @@ import SubTitle from '@/components/ui/SubTitle'
 import Input from '@/components/ui/Input'
 import Page from '@/components/ui/Page'
 import PageContent from '@/components/ui/PageContent'
-// import testUser from '@/data/studydata'
 
 export default {
   props: ['title'],
   data: () => ({
-    userIndex : testUser.study.index,
-    userName : testUser.study.myName,
-    famMembers : testUser.study.famMembers
+    famMembers: []
   }),
-  mounted: () => {
-    console.log(index),
-    console.log(myName),
-    console.log(famMembers)
+  mounted () {
+    this.$http.get('https://jsonplaceholder.typicode.com/users/')
+      .then((response) => {
+        console.log(response.data)
+        this.famMembers = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   },
- name: 'Study',
- components: {
-   Button, Title, SubTitle, Input, Page, PageContent
- },
- methods: {
-   addNewTodo: function () {
-     this.todos.push({
-       id: this.nextTodoId++,
-       title: this.newTodoText
-     })
-     this.newTodoText = ''
-   }
- },
- // computed: {
- //    page: function () {
- //      if (this.$route.query && this.$route.query.page) {
- //        return parseInt(this.$route.query.page)
- //      } else {
- //        return 1
- //      }
- //    },
- //    prevButtonState: function () {
- //      return this.page === 1 ? 'disabled' : 'default'
- //    },
- //    nextButtonState: function () {
- //      return 'primary'
- //    }
- //  }
+  name: 'Study',
+  components: {
+    Button, Title, SubTitle, Input, Page, PageContent
+  },
+  methods: {
+    addNewTodo: function () {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText
+      })
+      this.newTodoText = ''
+    }
+  }
+  // computed: {
+  //   page: function () {
+  //     if (this.$route.query && this.$route.query.page) {
+  //       return parseInt(this.$route.query.page)
+  //     } else {
+  //       return 1
+  //     }
+  //   },
+  //   prevButtonState: function () {
+  //     return this.page === 1 ? 'disabled' : 'default'
+  //   },
+  //   nextButtonState: function () {
+  //     return 'primary'
+  //   }
+  // }
 }
 </script>
